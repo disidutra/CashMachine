115ba24ddf0390deb63b03ec39198869e56130bf
OEM Configuration (temporary user) <di.si.dutra@gmail.com>
2019-11-28 22:23:59 -0300

    Concluido aplicaçao

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace CashMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int resto = 1;
+            var notas = new int[4];
+            var quantNotas = new int[4];
+            notas[0] = 100;
+            notas[1] = 50;
+            notas[2] = 20;
+            notas[3] = 10;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Bem vindo ao Caixa eletrônico!");
+                    Console.WriteLine("Notas disponíveis para saque:");
+                    Console.WriteLine("R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00");
+                    Console.WriteLine("");
+                    Console.WriteLine("Digite o valor que deseja sacar, deve ser um número inteiro:");
+                    resto = Int32.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < notas.Count() && resto > 0; i++)
+                    {
+                        if (resto >= notas[i])
+                        {
+                            quantNotas[i] = Convert.ToInt32(resto / notas[i]);
+                            resto = resto % notas[i];
+                        }
+                    }
+
+                    Console.WriteLine();
+                    if (resto > 0)
+                    {
+                        Console.WriteLine("Valor desejado para saque não é compatível com as notas disponíveis!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Você vai receber a quantidade de notas abaixo:");
+                        for (int i = 0; i < quantNotas.Count(); i++)
+                        {
+                            if(quantNotas[i] == 0) continue;
+                            Console.WriteLine($"R$ {notas[i]},00 => {quantNotas[i]}");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Deseja efetuar um novo saque:");
+                    Console.WriteLine("1 => Sim");
+                    Console.WriteLine("2 => Não");
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Algo aconteceu de errado!");
+                    Console.WriteLine("Deseja efetuar um novo saque:");
+                    Console.WriteLine("1 => Sim");
+                    Console.WriteLine("2 => Não");
+                }
+
+            } while (Console.ReadLine() == "1");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CashMachine
 Simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico. Console Application em .Net Core
+
+Os requisitos básicos são os seguintes:
+
+Entregar o menor número de notas;
+É possível sacar o valor solicitado com as notas disponíveis;
+Saldo do cliente infinito;
+Quantidade de notas infinito (pode-se colocar um valor finito de cédulas para aumentar a dificuldade do problema);
+Notas disponíveis de 100,00, 50,00, 20,00 e 10,00 reais
+Exemplos:
+
+Valor do Saque: 30,00 reais – Resultado Esperado: Entregar 1 nota de 20,00, reais e 1 nota de 10,00 reais.
+Valor do Saque: 80,00 reais – Resultado Esperado: Entregar 1 nota de 50,00 reais, 1 nota de 20,00 reais e 1 nota de 10,00 reais.
+
+---
+### Aplicação rodando no terminal
+<img src="./CashMachine.png" />
+
+Para rodar o projeto é necessário:
+-----------
+* .net core 3.0
+
+* Executar o comando a seguir no terminal dentro da pasta raíz do projeto para rodar a aplicação:
+```
+dotnet run
+```
